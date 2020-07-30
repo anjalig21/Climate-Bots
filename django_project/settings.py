1691cad2fb8e768043328749b02b28420b7bccca
anjalig21 <anjaligta@outlook.com>
2020-07-30 16:20:44 -0400

    edited settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -134,5 +134,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = #email
-EMAIL_HOST_PASSWORD = #password
+EMAIL_HOST_USER = 'climate.bots@gmail.com'
+EMAIL_HOST_PASSWORD = 'climatebots123@'
